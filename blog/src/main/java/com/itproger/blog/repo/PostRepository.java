8c05afaf7be8401db9201b114ed2572094f7e16d
Alexandr Gopienko <sagopien04@mail.ru>
2023-08-27 00:13:36 +0700

    blog

---
+++ blog/src/main/java/com/itproger/blog/repo/PostRepository.java
@@ -0,0 +1,6 @@
+package com.itproger.blog.repo;
+
+import org.springframework.data.repository.CrudRepository;
+import com.itproger.blog.models.Post;
+
+public interface PostRepository extends CrudRepository<Post, Long> { }
